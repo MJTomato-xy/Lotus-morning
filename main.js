c5b6e1c686f4de7913936be1c5586928f54a7dba
Xinyi-Liu-024 <59549169+Xinyi-Liu-024@users.noreply.github.com>
2023-05-22 19:12:14 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,140 @@
+// Add music
+var sound;
+
+function playsound(soundFile) {
+  sound = new Audio(soundFile);
+  sound.play();
+}
+  
+function createCircles1() {
+  var button = document.getElementById("lotus-btn1");
+  // Create small div elements
+  for (var i = 0; i < 20; i++) {
+    var circle = document.createElement("div");
+    circle.classList.add("circle");
+    document.getElementById("circle-container1").appendChild(circle);
+    var x = Math.floor(Math.random() * 600) - 300;
+    var y = Math.floor(Math.random() * 600) - 300;
+    circle.style.setProperty('--x', x + "px");
+    circle.style.setProperty('--y', y + "px");
+  }
+}
+
+function createCircles2() {
+  var button = document.getElementById("lotus-btn2");
+  // Create small div elements
+  for (var i = 0; i < 20; i++) {
+    var circle = document.createElement("div");
+    circle.classList.add("circle");
+    document.getElementById("circle-container2").appendChild(circle);
+    var x = Math.floor(Math.random() * 600) - 300;
+    var y = Math.floor(Math.random() * 600) - 300;
+    circle.style.setProperty('--x', x + "px");
+    circle.style.setProperty('--y', y + "px");
+  }
+}
+
+function createCircles3() {
+  var button = document.getElementById("lotus-btn3");
+  // Create small div elements
+  for (var i = 0; i < 20; i++) {
+    var circle = document.createElement("div");
+    circle.classList.add("circle");
+    document.getElementById("circle-container3").appendChild(circle);
+    var x = Math.floor(Math.random() * 600) - 300;
+    var y = Math.floor(Math.random() * 600) - 300;
+    circle.style.setProperty('--x', x + "px");
+    circle.style.setProperty('--y', y + "px");
+  }
+}
+
+function createCircles4() {
+  var button = document.getElementById("lotus-btn4");
+  // Create small div elements
+  for (var i = 0; i < 20; i++) {
+    var circle = document.createElement("div");
+    circle.classList.add("circle");
+    document.getElementById("circle-container4").appendChild(circle);
+    var x = Math.floor(Math.random() * 600) - 300;
+    var y = Math.floor(Math.random() * 600) - 300;
+    circle.style.setProperty('--x', x + "px");
+    circle.style.setProperty('--y', y + "px");
+  }
+}
+
+function createCircles5() {
+  var button = document.getElementById("lotus-btn5");
+  // Create small div elements
+  for (var i = 0; i < 20; i++) {
+    var circle = document.createElement("div");
+    circle.classList.add("circle");
+    document.getElementById("circle-container5").appendChild(circle);
+    var x = Math.floor(Math.random() * 600) - 300;
+    var y = Math.floor(Math.random() * 600) - 300;
+    circle.style.setProperty('--x', x + "px");
+    circle.style.setProperty('--y', y + "px");
+  }
+}
+
+function createCircles6() {
+  var button = document.getElementById("lotus-btn6");
+  // Create small div elements
+  for (var i = 0; i < 20; i++) {
+    var circle = document.createElement("div");
+    circle.classList.add("circle");
+    document.getElementById("circle-container6").appendChild(circle);
+    var x = Math.floor(Math.random() * 600) - 300;
+    var y = Math.floor(Math.random() * 600) - 300;
+    circle.style.setProperty('--x', x + "px");
+    circle.style.setProperty('--y', y + "px");
+  }
+}
+
+
+function createCircles7() {
+  var button = document.getElementById("lotus-btn7");
+  // Create small div elements
+  for (var i = 0; i < 20; i++) {
+    var circle = document.createElement("div");
+    circle.classList.add("circle");
+    document.getElementById("circle-container7").appendChild(circle);
+    var x = Math.floor(Math.random() * 600) - 300;
+    var y = Math.floor(Math.random() * 600) - 300;
+    circle.style.setProperty('--x', x + "px");
+    circle.style.setProperty('--y', y + "px");
+  }
+}
+
+function createCircles8() {
+  var button = document.getElementById("lotus-btn8");
+  // Create small div elements
+  for (var i = 0; i < 20; i++) {
+    var circle = document.createElement("div");
+    circle.classList.add("circle");
+    document.getElementById("circle-container8").appendChild(circle);
+    var x = Math.floor(Math.random() * 600) - 300;
+    var y = Math.floor(Math.random() * 600) - 300;
+    circle.style.setProperty('--x', x + "px");
+    circle.style.setProperty('--y', y + "px");
+  }
+}
+
+function createCircles9() {
+  var button = document.getElementById("lotus-btn9");
+  // Create small div elements
+  for (var i = 0; i < 20; i++) {
+    var circle = document.createElement("div");
+    circle.classList.add("circle");
+    document.getElementById("circle-container9").appendChild(circle);
+    var x = Math.floor(Math.random() * 600) - 300;
+    var y = Math.floor(Math.random() * 600) - 300;
+    circle.style.setProperty('--x', x + "px");
+    circle.style.setProperty('--y', y + "px");
+  }
+}
+
+
+  
+  
+  
+  
